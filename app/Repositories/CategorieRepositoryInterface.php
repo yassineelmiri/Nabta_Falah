18882dba1642ff8effd41ca0739795dc2d2229c6
yassineelmiri <yassireelhima@gmail.com>
2024-12-04 16:38:34 +0100

    Ajout de CategorieRepositoryInterface.php

---
+++ app/Repositories/CategorieRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+interface CategorieRepositoryInterface
+{
+    public function getAll();
+    public function getById($id);
+    public function create(array $data);
+    public function update($id, array $data);
+    public function delete($id);
+    public function getByAttributes($attributes);
+}
