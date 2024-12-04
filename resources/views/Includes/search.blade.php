a84806a07701082141c3686b141d9a7494040c1d
yassineelmiri <yassireelhima@gmail.com>
2024-12-04 16:41:15 +0100

    Ajout de search.blade.php

---
+++ resources/views/Includes/search.blade.php
@@ -0,0 +1,13 @@
+<div class="w-full mx-auto ">
+    <form>
+        <div class="relative w-full max-w-xl mx-auto bg-white rounded-full  my-2">
+            <input placeholder="flah" class="rounded-full w-full h-10 bg-transparent py-2 pl-8 pr-32 outline-none border-2 border-gray-100 shadow-md hover:outline-none focus:ring-green-200 focus:border-green-200" type="text" name="query" id="query">
+            <button type="submit" class="absolute inline-flex items-center h-5  py-2 text-sm text-white transition duration-150 ease-in-out rounded-full outline-none right-3 top-3 bg-green-700 sm:px-6 sm:text-base sm:font-medium hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+              <svg class="-ml-0.5 sm:-ml-1 mr-2 w-4 h-4 sm:h-5 sm:w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
+              Search
+            </button>
+          </div>
+    </form>
+</div>
