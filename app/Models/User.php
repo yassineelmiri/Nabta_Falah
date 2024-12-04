3477b0ef73c67e8a8b179c82a3039f17becc7935
yassineelmiri <yassireelhima@gmail.com>
2024-12-04 16:38:52 +0100

    Ajout de User.php

---
+++ app/Models/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'type',
+        'region',
+        'ville',
+        'adress',
+        'role_id',
+        'tele',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function demande()
+{
+    return $this->hasOne(Demande::class);
+}
+
+public function panier()
+{
+    return $this->hasOne(Panier::class);
+}
+
+
+public function produit(){
+    return $this->hasMany(Produit::class);
+}
+}
