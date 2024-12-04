f4350d13ef5aac44f1f8791d4536535db9f3512a
yassineelmiri <yassireelhima@gmail.com>
2024-12-04 16:40:31 +0100

    Ajout de dashboard.blade.php

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,35 @@
+
+@include('includes.header')
+<body class="text-gray-800 font-inter">
+    <!--sidenav -->
+    @include('admin.sidebare')
+    <!-- end sidenav -->
+    <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-white-200 min-h-screen transition-all main">
+    <!-- navbar -->
+     @include('admin.profilenav')
+    <!-- end navbar -->
+
+      <!-- Content -->
+        <div class="p-6" >
+
+            {{-- @include('admin.statistique') --}}
+            {{-- @include('admin.categorie-create') --}}
+
+            @yield('content')
+        </div>
+      <!-- End Content -->
+    </main>
+
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
+    <script src="{{ asset('js/fullscreen.js') }}"></script>
+    <script src="{{ asset('js/sidebar.js') }}"></script>
+    <script src="{{ asset('js/popper.js') }}"></script>
+    {{-- <script src="{{ asset('js/chart.js') }}"></script> --}}
+    <script src="{{ asset('js/tab.js') }}"></script>
+     
+     
+    
+
+</body>
+</html>
