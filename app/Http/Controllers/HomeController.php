ba05ab18c2eecc70319938116e681c8c06871706
yassineelmiri <yassireelhima@gmail.com>
2024-12-04 16:39:41 +0100

    Ajout de HomeController.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\CategorieService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Produit;
+use App\Models\Image;
+use App\Models\Categorie;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller{
+
+  public function home(){
+
+    
+
+  }
+
+
+
+
+
+}
